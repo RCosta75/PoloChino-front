789da807ca6799eaa72046b55316350b6e6524f9
Toure Lassana <l.toure910@laposte.net>
2024-10-25 17:07:42 +0200

    first commit

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+
+import styles from '../styles/Header.module.css';
+
+
+
+
+
+
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.left}>
+              <img src='hfb'/>
+              <span>Shop</span>
+              <span>About</span>
+            </div>
+            <div className={styles.right}>
+            <input type="text" placeholder="Search"/>
+            <h5>icon</h5>
+            <h5>icon</h5>
+            <h5>icon</h5>
+            </div>
+           
+        </header>
+
+    )
+}
+export default Header;
--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,24 @@
 import styles from '../styles/Home.module.css';
+import Header from './Header';
+import Head from 'next/head';
+
+
 
 function Home() {
+  
   return (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+       <Head>
+        <title>POL-HO - Home</title>
+      </Head>
+      <Header />
+      <div className={styles.poloContainer}>
+        {/* Nos articles polo */}
+        <h1>polo</h1>
+        <h1>polo</h1>
+        <h1>polo</h1>
+        <h1>polo</h1>
+      </div>
     </div>
   );
 }
