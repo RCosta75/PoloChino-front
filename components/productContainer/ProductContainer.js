64c014f9f93919edc49fea47bcaa4cfe572d08ef
Your name <Your email>
2024-10-28 16:11:50 +0100

    raph front

--- components/productContainer/ProductContainer.js
+++ components/productContainer/ProductContainer.js
@@ -2,54 +2,7 @@ import React from "react";
 import Card from "./Card";
 import { useEffect, useState } from "react";
 
-/*
-const poloInfo = [
-  {
-    id: 1,
-    image: "/TestPolo (1).jpg",
-    description: "Cotton Polo Shirt with Collar and Pocket",
-    price: 24.99,
-  },
-  {
-    id: 2,
-    image: "/TestPolo (2).jpg",
-    description: "Pima Cotton Polo Shirt with V-Neck and Sleeves",
-    price: 29.99,
-  },
-  {
-    id: 3,
-    image: "/TestPolo (3).jpg",
-    description: "Performance Polo Shirt with Moisture Wicking Fabric",
-    price: 34.99,
-  },
-  {
-    id: 4,
-    image: "/TestPolo (4).jpg",
-    description: "Premium Polo Shirt with Stretch Fabric and Pocket",
-    price: 39.99,
-  },
-  {
-    id: 5,
-    image: "/TestPolo (5).jpg",
-    description: "Premium Polo Shirt with Stretch Fabric and Pocket",
-    price: 39.99,
-  },
 
-  {
-    id: 6,
-    image: "/TestPolo (6).jpg",
-    description: "Premium Polo Shirt with Stretch Fabric and Pocket",
-    price: 39.99,
-  },
-
-  {
-    id: 7,
-    image: "/TestPolo (7).jpg",
-    description: "Premium Polo Shirt with Stretch Fabric and Pocket",
-    price: 39.99,
-  },
-];
-*/
 
 export default function ProductContainer() {
 
