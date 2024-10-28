9caa2a3d52c82cb1955bc8fde2feee5750e31dc9
Your name <Your email>
2024-10-28 12:40:52 +0100

    change cart redux

--- reducers/cart.js
+++ reducers/cart.js
@@ -1,27 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 // reducer pour contenir les polo dans le panier
 
-
 const initialState = {
-    value: [ ],
-  };
+  value: [null],
+};
 
 export const trendSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      state.value.push(action.payload)
+      state.value.push(action.payload);
     },
-    removeCart : (state, action) => {
-        state.value.filter((e) => {e === action.payload})
+    removeCart: (state, action) => {
+      state.value.filter((e) => e === action.payload);
     },
-    cleanCart : (state, action) => {
-        state.value = [null]
-    }
+    clearCart: (state, action) => {
+      state.value = [null];
+    },
+    addQuantity: (state, action) => {
+      state.value.quantity += 1;
+    },
+    suppQuantity: (state, action) => {
+      state.value.quantity -= 1;
+    },
   },
 });
 
-export const { addToCart, removeCart, cleanCart } = trendSlice.actions;
+export const { addToCart, removeCart, clearCart, addQuantity, suppQuantity } 
+= trendSlice.actions;
 export default trendSlice.reducer;
