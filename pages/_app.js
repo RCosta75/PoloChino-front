6341c224deae4bd8a56b7087ec864f23e00d2f61
Your name <Your email>
2024-10-28 11:41:12 +0100

    tkt

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
+import cart from '../reducers/cart';
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+
+
+const store = configureStore({
+  reducer: {cart},
+ });
+
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Next.js App</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
