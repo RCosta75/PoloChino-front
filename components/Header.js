8de090716a4ccc30bd4a9431c0072f9e77b975c1
Your name <Your email>
2024-10-29 16:43:17 +0100

    raph redux

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,34 @@
-import styles from '../styles/Header.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import React, { useState } from 'react';
-import { logout } from '../reducers/user';
-import { useDispatch, useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
+import styles from "../styles/Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faCartShopping,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { logout } from "../reducers/user";
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import { Popover } from "antd";
+import CartCard from "./Cart/CartCard";
 
 function Header({ setSearchTerm }) {
   //Passez le terme de recherche et la fonction de mise à jour de l'état de recherche au composant Header.
   const dispatch = useDispatch();
   const router = useRouter();
-  const user = useSelector(state => state.user.value);
+  const user = useSelector((state) => state.user.value);
+  const cart = useSelector((state) => state.cart.value);
   const [showLogout, setShowLogout] = useState(false); // On ne l'affiche pas initialement
   const [showSearch, setShowSearch] = useState(false); // On ne l'affiche pas initialement
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
 
+  const handleSearch = () => {
+    setSearchTerm(searchInput);
+  };
 
-  const handleSearch = () => { setSearchTerm(searchInput); };
-
+  const poloProduct = cart?.map((polo, i) => {
+    return <CartCard key={i} polo={polo} className={styles.headerCart}/>;
+  });
 
   const handleLogout = () => {
     dispatch(logout());
@@ -25,7 +36,7 @@ function Header({ setSearchTerm }) {
   };
 
   const redirectToLogin = () => {
-    router.push('/login'); // Rediriger vers la page de connexion
+    router.push("/login"); // Rediriger vers la page de connexion
   };
 
   const toggleSearch = () => {
@@ -39,30 +50,52 @@ function Header({ setSearchTerm }) {
       redirectToLogin(); // Rediriger vers la page de connexion si non connecté
     }
   };
- 
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>{poloProduct}</div>
+  );
+
   return (
     <header className={styles.header}>
       <div className={styles.left}>
-        <img src='poloLogo.png' alt='logo' onClick={() => router.push('/')}/>
+        <img src="poloLogo.png" alt="logo" onClick={() => router.push("/")} />
         <span>Shop</span>
         <span>About</span>
       </div>
       <div className={styles.right}>
-        {showSearch && <input type="text" placeholder="Search" className={styles.searchInput} 
-        value={searchInput} 
-        onChange={(e) => setSearchInput(e.target.value)} />}
+        {showSearch && (
+          <input
+            type="text"
+            placeholder="Search"
+            className={styles.searchInput}
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        )}
         <button onClick={handleSearch}>Go</button>
         <FontAwesomeIcon icon={faMagnifyingGlass} onClick={toggleSearch} />
-        <FontAwesomeIcon icon={faCartShopping} onClick={() => router.push('/cart')}/>
-          <FontAwesomeIcon icon={faUser} onClick={handleUserClick} />
-         
-          
-          {showLogout && (
-           <div className={styles.btn}>
-             <button className={styles.log} onClick={handleLogout}>Logout</button>
-             </div>
-          )}
-       
+
+        <Popover
+          overlayStyle={{ maxWidth: "300px", maxHeight: '30vh' }}
+          placement="bottom"
+          title="My Cart"
+          content={popoverContent}
+        >
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            onClick={() => router.push("/cart")}
+          />
+        </Popover>
+
+        <FontAwesomeIcon icon={faUser} onClick={handleUserClick} />
+
+        {showLogout && (
+          <div className={styles.btn}>
+            <button className={styles.log} onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        )}
       </div>
     </header>
   );
