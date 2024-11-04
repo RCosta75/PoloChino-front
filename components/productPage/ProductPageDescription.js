7bf55d963bcd1e074e70ced7297d8dfe319d1bf8
Your name <Your email>
2024-11-04 11:35:39 +0100

    raph debug

--- components/productPage/ProductPageDescription.js
+++ components/productPage/ProductPageDescription.js
@@ -8,7 +8,7 @@ export default function ProductPageDescription({ product }) {
   return (
     <div className="w-1/3 pl-20 gap-10 text-xl  flex flex-col justify-center  ">
       <ProductDetailsHeader product={product} />
-    <SizeColorSelect/>
+    <SizeColorSelect product={product}/>
     </div>
   );
 }
