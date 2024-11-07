cc8ec24561ff35051c13191ee0d7b281fd92da77
Toure Lassana <l.toure910@laposte.net>
2024-11-07 16:37:00 +0100

    commit

--- components/UpdateInformation.js
+++ components/UpdateInformation.js
@@ -58,7 +58,7 @@ function UserProfile() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-1/2 h-screen hidden lg:block">
-        <img src="TestPolo (3).jpg" alt="Your Image Alt Text"/>
+        <img src="TestPolo (2).jpg" alt="Your Image Alt Text" className="h-screen w-full"/>
       </div>
       <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
         <h1 className="text-2xl font-semibold mb-4">Informations</h1>
